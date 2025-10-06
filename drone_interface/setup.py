beac936d5932657018d218138dfe2aa6cf31c80e
Victor <c25victor.chen@afacademy.af.edu>
2025-10-06 10:13:34 -0600

    Framework Setup

---
+++ drone_interface/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'drone_interface'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Dr. Baek',
+    maintainer_email='c26ty.hubert@afacademy.af.edu',
+    description='Robot Teaming Capstone',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'controller = drone_interface.controller:main',
+        ],
+    },
+)
